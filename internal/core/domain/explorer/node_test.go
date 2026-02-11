95986b401a1c4ce73ced98a66f1fb3258fda082b
rm-ryou <rmco.420118@gmail.com>
2026-02-11 19:22:28 +0900

    refactor: node test

--- internal/core/domain/explorer/node_test.go
+++ internal/core/domain/explorer/node_test.go
@@ -2,117 +2,100 @@ package explorer
 
 import "testing"
 
-func Test_Flatten_FileNode(t *testing.T) {
-	t.Parallel()
-
-	var act []*Node
-	n := &Node{
-		Name:  "main.go",
-		Path:  "/path/main.go",
-		IsDir: false,
+func Test_Flatten(t *testing.T) {
+	type testCase struct {
+		name      string
+		node      *Node
+		wantNames []string
 	}
 
-	n.Flatten(&act)
-
-	if len(act) != 1 {
-		t.Fatalf("want: 1, act: %d", len(act))
-	}
-	if act[0].Name != n.Name {
-		t.Errorf("want: %s, act: %s", n.Name, act[0].Name)
-	}
-}
-
-func Test_Flatten_CollapsedDir(t *testing.T) {
-	t.Parallel()
-
-	var act []*Node
-	dir := &Node{
-		Name:     "internal",
-		Path:     "/path/internal",
-		IsDir:    true,
-		Expanded: false,
-		Children: []*Node{
-			{Name: "hoge.go", Path: "/path/internal/hoge.go"},
-		},
-	}
-
-	dir.Flatten(&act)
-
-	if len(act) != 1 {
-		t.Fatalf("want: 1, act: %d", len(act))
-	}
-	if act[0].Name != dir.Name {
-		t.Errorf("want: %s, act: %s", dir.Name, act[0].Name)
-	}
-}
-
-func Test_Flatten_ExpandedDir(t *testing.T) {
-	t.Parallel()
-
-	var act []*Node
-	dir := &Node{
-		Name:     "internal",
-		Path:     "path/internal",
-		IsDir:    true,
-		Expanded: true,
-		Children: []*Node{
-			{
-				Name:     "core",
-				Path:     "/path/internal/core",
-				IsDir:    true,
-				Expanded: true,
-				Children: []*Node{
-					{Name: "domain.go", Path: "/path/internal/core/domain.go"},
-				},
+	testCases := []testCase{
+		{
+			name: "File node returns itself",
+			node: &Node{
+				Name:  "main.go",
+				Path:  "/path/main.go",
+				IsDir: false,
+				Depth: 0,
 			},
-			{Name: "hoge.go", Path: "/path/internal/hoge.go"},
+			wantNames: []string{"main.go"},
 		},
-	}
-
-	dir.Flatten(&act)
-
-	wantNames := []string{"internal", "core", "domain.go", "hoge.go"}
-	if len(act) != len(wantNames) {
-		t.Fatalf("want: %d, act: %d", len(wantNames), len(act))
-	}
-	for i, name := range wantNames {
-		if act[i].Name != name {
-			t.Errorf("want: %s, act[%d].Name: %s", name, i, act[i].Name)
-		}
-	}
-}
-
-func Test_Flatten_NestedCollapsed(t *testing.T) {
-	t.Parallel()
-
-	var act []*Node
-	dir := &Node{
-		Name:     "root",
-		Path:     "/path",
-		IsDir:    true,
-		Expanded: true,
-		Children: []*Node{
-			{
+		{
+			name: "Collapsed directory returns only itself",
+			node: &Node{
 				Name:     "internal",
 				Path:     "/path/internal",
 				IsDir:    true,
+				Depth:    0,
 				Expanded: false,
 				Children: []*Node{
-					{Name: "hoge.go", Path: "/path/internal/hoge.go"},
+					{Name: "hoge.go", Path: "/path/internal/hoge.go", Depth: 1},
 				},
 			},
+			wantNames: []string{"internal"},
+		},
+		{
+			name: "Expanded directory returns itself and child nodes",
+			node: &Node{
+				Name:     "internal",
+				Path:     "path/internal",
+				IsDir:    true,
+				Depth:    0,
+				Expanded: true,
+				Children: []*Node{
+					{
+						Name:     "core",
+						Path:     "/path/internal/core",
+						IsDir:    true,
+						Depth:    1,
+						Expanded: true,
+						Children: []*Node{
+							{Name: "domain.go", Path: "/path/internal/core/domain.go", Depth: 2},
+						},
+					},
+					{Name: "hoge.go", Path: "/path/internal/hoge.go", Depth: 1},
+				},
+			},
+			wantNames: []string{"internal", "core", "domain.go", "hoge.go"},
+		},
+		{
+			name: "If collapsed midway, nodes below are excluded",
+			node: &Node{
+				Name:     "root",
+				Path:     "/path",
+				IsDir:    true,
+				Depth:    0,
+				Expanded: true,
+				Children: []*Node{
+					{
+						Name:     "internal",
+						Path:     "/path/internal",
+						IsDir:    true,
+						Depth:    1,
+						Expanded: false,
+						Children: []*Node{
+							{Name: "hoge.go", Path: "/path/internal/hoge.go", Depth: 2},
+						},
+					},
+				},
+			},
+			wantNames: []string{"root", "internal"},
 		},
 	}
 
-	dir.Flatten(&act)
+	for _, tc := range testCases {
+		t.Run(tc.name, func(t *testing.T) {
+			var act []*Node
 
-	wantNames := []string{"root", "internal"}
-	if len(act) != len(wantNames) {
-		t.Fatalf("want: %d, act: %d", len(wantNames), len(act))
-	}
-	for i, name := range wantNames {
-		if act[i].Name != name {
-			t.Errorf("want: %s, act[%d].Name: %s", name, i, act[i].Name)
-		}
+			tc.node.Flatten(&act)
+			if len(act) != len(tc.wantNames) {
+				t.Fatalf("want: %d, act: %d", len(tc.wantNames), len(act))
+			}
+			for i, name := range tc.wantNames {
+				if act[i].Name != name {
+					t.Errorf("want: %s, act[%d].Name: %s", name, i, act[i].Name)
+				}
+			}
+		})
 	}
 }
