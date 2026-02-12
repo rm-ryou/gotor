60525dd539c0cf3275471600bf6150e0dbbb6c33
rm-ryou <rmco.420118@gmail.com>
2026-02-12 23:34:04 +0900

    feat: add simple window

--- cmd/gotor/main.go
+++ cmd/gotor/main.go
@@ -1,7 +1,16 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"os"
+
+	"github.com/rm-ryou/gotor/internal/ui/gio"
+)
 
 func main() {
-	fmt.Println("Hello, gotor!")
+	app := gio.New()
+	if err := app.Run(); err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
 }
