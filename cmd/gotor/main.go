b144577465c46c039d0219ec7225e38ab9ff3dd4
rm-ryou <rmco.420118@gmail.com>
2026-02-14 05:18:28 +0900

    feat: add bg color

--- cmd/gotor/main.go
+++ cmd/gotor/main.go
@@ -4,12 +4,21 @@ import (
 	"fmt"
 	"os"
 
-	"github.com/rm-ryou/gotor/internal/ui/gio"
+	"github.com/rm-ryou/gotor/internal/core/usecase"
+	"github.com/rm-ryou/gotor/internal/platform/fs"
+	"github.com/rm-ryou/gotor/internal/ui/gio/app"
 )
 
 func main() {
-	app := gio.New()
-	if err := app.Run(); err != nil {
+	// TODO: refactor
+	fs := fs.New(true)
+	explorerUC, err := usecase.NewExplorer(fs, "")
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+
+	if err := app.New(explorerUC).Run(); err != nil {
 		fmt.Println(err)
 		os.Exit(1)
 	}
